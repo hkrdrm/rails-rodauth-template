2a337e9795209c56408287f9180b198991014c38
Matt Spiers <zerosum022@gmail.com>
2025-09-11 13:01:38 -0500

    not sure where i left off think this works

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,18 @@
 class Issue < Sequel::Model(:comic_issues)
   many_to_one :comic
 
+  def previous_issue
+    id - 1
+  end
+
+  def next_issue
+    if Issue.last.id == id
+      return 1
+    else
+      return id + 1
+    end
+  end
+
   def save_image(file)
     filename = SecureRandom.hex(10) + ".png"
 
